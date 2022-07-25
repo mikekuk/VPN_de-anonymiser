4a677c8f0455382074eeaad1075f877e427b6685
Mike <mikekearney85@hotmail.com>
2022-07-25 19:08:30 +0100

    Imporvments ot real time capture

--- VPN_router_scripts/real_time_capture.py
+++ VPN_router_scripts/real_time_capture.py
@@ -67,7 +67,7 @@ def gen_matrix():
 def predict(matrix):
     predictions = model.predict(matrix/SCALER_MAX)
     print("\n\n")
-    print(f"[+] Prediction = {SITES[np.argmax(predictions[0])]}")
+    print(f"[+] Prediction = {SITES[np.argmax(predictions[0])]}\n")
     for i in range(len(SITES)):
         print(f"{SITES[i]} - {round(predictions[0][i], 2)}")
     print("\n\n")
