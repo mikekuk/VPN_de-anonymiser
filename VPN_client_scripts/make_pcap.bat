69a8bc65504cbb379ec4bd5ed148f48be8428313
mikekuk <54645026+mikekuk@users.noreply.github.com>
2023-03-12 12:40:49 +0000

    Create make_pcap.bat

---
+++ VPN_client_scripts/make_pcap.bat
@@ -0,0 +1,27 @@
+
+@echo off
+setlocal enableextensions
+
+REM Check if the script is running as administrator
+NET FILE >NUL 2>&1
+if '%errorlevel%' == '0' (goto START) else (goto UAC)
+
+:UAC
+REM Prompt the user to run the script as administrator
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B
+
+:START
+REM Check if an argument was provided
+if "%~1" == "" (
+  echo Usage: %0 output_file_name
+  exit /B
+)
+
+REM Run tshark with the specified arguments
+"C:\Program Files\Wireshark\tshark.exe" -i 1 -s 128 -a duration:20 -w "C:\Users\Administrator\Documents\pcaps\%~1.pcap" & exit
+
+:end
